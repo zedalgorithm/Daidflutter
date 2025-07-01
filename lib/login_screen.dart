db1852002238c7145b62d162929f7939905f822a
Ivanne Cariño <carinoivannejay@gmail.com>
2025-07-01 13:55:56 +0800

    #eto na to

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'package:sizer/sizer.dart';
 import 'signup_screen.dart'; 
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
@@ -51,181 +49,174 @@ class LoginScreen extends StatelessWidget {
       resizeToAvoidBottomInset: false,
       backgroundColor: Colors.white,
       body: SafeArea(
-        child: Center(
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                // Logo
-                Container(
-                  margin: EdgeInsets.only(top: 30.0),
-                  child: Image.asset('assets/logo.png', height: 250)
-                  ),
-                Container(
-                  margin: EdgeInsets.only(top: 10.0),
-                  child: const Text("Log In", 
-                  style: TextStyle(fontSize: 30.0, fontWeight: FontWeight.w800),),
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                margin: const EdgeInsets.only(top: 30.0),
+                child: Image.asset('assets/logo.png', height: 150)
                 ),
-                // Email TextField
-                Container(
-                  margin: EdgeInsets.only(top: 20.0),
-                  child: TextField(
-                    controller: emailController,
-                    decoration: const InputDecoration(
-                      labelText: 'Email',
-                      border: OutlineInputBorder(),
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 10),
-                
-                // Password TextField
-                TextField(
-                  controller: passwordController,
-                  obscureText: true,
+              Container(
+                margin: const EdgeInsets.only(top: 10.0),
+                child: const Text("Log In", 
+                style: TextStyle(fontSize: 30.0, fontWeight: FontWeight.w800),),
+               ),
+                                  // Email TextField
+              Container(
+                margin: const EdgeInsets.only(top: 20.0),
+                child: TextField(
+                  controller: emailController,
                   decoration: const InputDecoration(
-                    labelText: 'Password',
+                    labelText: 'Email',
                     border: OutlineInputBorder(),
                   ),
                 ),
-                // const SizedBox(height: double.maxFinite),
-                Container(
-                  margin: EdgeInsets.only(top: 5.0),
-                  child: Align(
-                    alignment: FractionalOffset.topRight,
-                    child: TextButton(
-                      style: TextButton.styleFrom(
-                        minimumSize: Size.zero,
-                        padding: EdgeInsets.zero,
-                        tapTargetSize: MaterialTapTargetSize.shrinkWrap,
-                      ),
-                      onPressed: () {
-                        // Navigator.push(
-                        //   // context,
-                        //   // // MaterialPageRoute(builder: (context) => const SignUpScreen()),
-                        // );
-                      },
-                      child: const Text('Forget Password.', 
-                      style: TextStyle(color: Colors.indigoAccent),
-                      ),
-                    ),
-                  ),
-                ),
-               
-                
-                // Login Button
-                Expanded(
-                  child: Align(
-                    alignment: FractionalOffset.bottomCenter,
-                    child: ElevatedButton(
-                      onPressed: () async {
-                        try {
-                          UserCredential userCredential = await FirebaseAuth.instance.signInWithEmailAndPassword(
-                            email: emailController.text.trim(),
-                            password: passwordController.text.trim(),
-                          );
-                          
-                          // Get user data from Firestore
-                          DocumentSnapshot userDoc = await FirebaseFirestore.instance
-                              .collection('Accounts')
-                              .doc(userCredential.user!.uid)
-                              .get();
+              ),
+              const SizedBox(height: 10),
                                   
-                          if (userDoc.exists) {
-                            String userType = userDoc.get('userType');
-                            
-                            // Show success dialog first
-                            await _showDialog(
-                              context,
-                              'Success',
-                              'Login successful!',
-                              false
-                            );
-                            
-                            // Navigate based on userType
-                            if (userType == 'Admin') {
-                              Navigator.pushReplacement(
-                                context,
-                                MaterialPageRoute(builder: (context) => const AdminPage()),
-                              );
-                            } else if (userType == 'User') {
-                              Navigator.pushReplacement(
-                                context,
-                                MaterialPageRoute(builder: (context) => const UserMainPage()),
-                              );
-                            }
-                          } else {
-                            await _showDialog(
-                              context,
-                              'Error',
-                              'User document not found',
-                              true
-                            );
-                          }
-                        } on FirebaseAuthException catch (e) {
-                          if (e.code == 'user-not-found') {
-                            await _showDialog(
-                              context,
-                              'Login Error',
-                              'No user found for that email',
-                              true
-                            );
-                          } else if (e.code == 'wrong-password') {
-                            await _showDialog(
-                              context,
-                              'Login Error',
-                              'Wrong password provided. Please try again.',
-                              true
-                            );
-                          } else {
-                            await _showDialog(
-                              context,
-                              'Login Error',
-                              'Error: ${ 'Wrong password provided. Please try again.'}',
-                              true
-                            );
-                          }
-                        } catch (e) {
-                          await _showDialog(
-                            context,
-                            'Error',
-                            'Error: ${e.toString()}',
-                            true
-                          );
-                        }
-                      },
-                      style: ElevatedButton.styleFrom(
-                        fixedSize: const Size(double.maxFinite, 50.0),
-                        backgroundColor: const Color(0xFFD71802),
-                        foregroundColor: Colors.white,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                      ),
-                      child: const Text('Sign In',
-                        style: TextStyle(
-                          fontSize: 16.0,
-                          fontWeight: FontWeight.w600
-                        )
-                          ),
+                                 // Password TextField
+              TextField(
+                controller: passwordController,
+                obscureText: true,
+                decoration: const InputDecoration(
+                  labelText: 'Password',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+                                  // const SizedBox(height: double.maxFinite),
+                Container(
+                margin: const EdgeInsets.only(top: 5.0),
+                child: Align(
+                  alignment: FractionalOffset.topRight,
+                  child: TextButton(
+                    style: TextButton.styleFrom(
+                      minimumSize: Size.zero,
+                      padding: EdgeInsets.zero,
+                      tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                    ),
+                    onPressed: () {
+                      // Navigator.push(
+                      //   // context,
+                      //   // // MaterialPageRoute(builder: (context) => const SignUpScreen()),
+                      // );
+                    },
+                    child: const Text('Forget Password.', 
+                    style: TextStyle(color: Colors.indigoAccent),
                     ),
                   ),
                 ),
-                Container(
-                  margin: EdgeInsets.only(top: 10.0),
-                  child: divider()
                   ),
-                 const Padding(
-                          padding: EdgeInsets.only(left: 25.0, right: 25.0, top: 12.0, bottom: 5.0),
-                          child: Center(
-                            child: Text('''Don't have an account yet?''', style: TextStyle(fontSize: 12.0, fontWeight: FontWeight.w600)),
-                          ),
-                        ),
-                createAccountBTN(context),
-                // Sign Up Link      
-              ],
-            ),
+                                const Spacer(),
+                              // Login Button
+                            Align(
+                              child: ElevatedButton(
+                                onPressed: () async {
+              try {
+                UserCredential userCredential = await FirebaseAuth.instance.signInWithEmailAndPassword(
+                  email: emailController.text.trim(),
+                  password: passwordController.text.trim(),
+                );
+                
+                // Get user data from Firestore
+                DocumentSnapshot userDoc = await FirebaseFirestore.instance
+                    .collection('Accounts')
+                    .doc(userCredential.user!.uid)
+                    .get();
+                        
+                if (userDoc.exists) {
+                  String userType = userDoc.get('userType');
+                  
+                  // Show success dialog first
+                  await _showDialog(
+                    context,
+                    'Success',
+                    'Login successful!',
+                    false
+                  );
+                  
+                  // Navigate based on userType
+                  if (userType == 'Admin') {
+                    Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(builder: (context) => const AdminPage()),
+                    );
+                  } else if (userType == 'User') {
+                    Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(builder: (context) => const UserMainPage()),
+                    );
+                  }
+                } else {
+                  await _showDialog(
+                    context,
+                    'Error',
+                    'User document not found',
+                    true
+                  );
+                }
+              } on FirebaseAuthException catch (e) {
+                if (e.code == 'user-not-found') {
+                  await _showDialog(
+                    context,
+                    'Login Error',
+                    'No user found for that email',
+                    true
+                  );
+                } else if (e.code == 'wrong-password') {
+                  await _showDialog(
+                    context,
+                    'Login Error',
+                    'Wrong password provided. Please try again.',
+                    true
+                  );
+                } else {
+                  await _showDialog(
+                    context,
+                    'Login Error',
+                    'Error: ${ 'Wrong password provided. Please try again.'}',
+                    true
+                  );
+                }
+              } catch (e) {
+                await _showDialog(
+                  context,
+                  'Error',
+                  'Error: ${e.toString()}',
+                  true
+                );
+              }
+                                },
+                                style: ElevatedButton.styleFrom(
+              fixedSize: const Size(double.maxFinite, 50.0),
+              backgroundColor: const Color(0xFFD71802),
+              foregroundColor: Colors.white,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+                                ),
+                                child: const Text('Sign In',
+              style: TextStyle(
+                fontSize: 16.0,
+                fontWeight: FontWeight.w600
+              )
+                ),
+                              ),
+                            ),
+                      Container(
+                            margin: const EdgeInsets.only(top: 10.0),
+                            child: divider()
+                            ),
+                       const Padding(
+                padding: EdgeInsets.only(left: 25.0, right: 25.0, top: 12.0, bottom: 5.0),
+                child: Center(
+                  child: Text('''Don't have an account yet?''', style: TextStyle(fontSize: 12.0, fontWeight: FontWeight.w600)),
+                ),
+              ),
+                      createAccountBTN(context),
+            ],
           ),
         ),
       ),
